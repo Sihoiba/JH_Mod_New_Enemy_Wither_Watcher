7b4329216061d6a93f5f0ee4587e3adcfef9589d
Sihoiba <dominic.decesare+github@gmail.com>
2023-10-22 23:09:03 +0100

    Making the wither watcher wither even if is doing nothing else

--- enemy_data/wither_watcher.lua
+++ enemy_data/wither_watcher.lua
@@ -1,79 +1,94 @@
 register_blueprint "wither_watcher"
 {
-	blueprint = "watcher_base",
-	lists = {
-		group    = "being",
-		-- { keywords = { "test" }, weight = 150 },
-		{ { "wither_watcher", "pyrowatcher" }, keywords = { "test" }, weight = 150 },
-		{ keywords = { "dante", "beyond", "hard", "demon", "demon2" }, weight = 300 },
-		{ { "wither_watcher", "pyrowatcher" }, keywords = { "dante", "beyond", "hard", "demon", "demon2" }, weight = 150 },
-	},
-	attributes = {
-		resist = {
-			acid   = 50,
-			toxin  = 50,
-			ignite = 50,
-			bleed  = 50,
-		},
-		experience_value = 100,
-		health           = 200,
-	},
-	text = {
-		name      = "wither watcher",
-		namep     = "wither watchers",
-	},
-	callbacks = {
-		on_create = [=[
-		function( self )
-		end
-		]=],
-		self_destruct = [=[
-			function( self )
-				if self:child("watcher_suicide") then
-					world:destroy( self:child("watcher_suicide") )
-					if self.health.current > 0 then	self.health.current = 1 end
-					local w = world:create_entity( "watcher_self_destruct" )
-					world:attach( self, w )
-					world:get_level():fire( self, world:get_position( self ), w, 200 )
-				end
-			end
-		]=],
-		on_die = [=[
-			function( self, killer, current, weapon )
-				if self:child("watcher_suicide") then
-					world:lua_callback( self, "self_destruct" )
-				end
-			end
-		]=],
-		on_action = [=[
-			function( self )
-				aitk.standard_ai( self ) 
-				local level    = world:get_level()
-				if level:is_alive( self ) and self:child( "watcher_suicide" ) then
-					local distance = level:distance( self, world:get_player() )
-					if distance < 2 then
-						world:lua_callback( self, "self_destruct" )
-					end
-				end
-			end
-		]=],
-		on_post_command = [=[
-			function ( actor, cmt, tgt, time )
-				local level = world:get_level()
-				for b in level:targets( actor, 8 ) do 
-					if b.data then
-						local data = b.data
-						if data.ai and not data.is_mechanical then
-							if data.ai.group == "player" or data.ai.group == "cri" then
-								world:add_buff( b, "buff_wither", 101, true )
-								core.apply_damage_status( b, "poisoned", "toxin", 1, actor )
-							else
-								world:add_buff( b, "buff_watcher_gaze_enemy", 101, true )
-							end
-						end
-					end
-				end
-			end
-		]=],
-	},
+    blueprint = "watcher_base",
+    lists = {
+        group    = "being",
+        -- { keywords = { "test" }, weight = 150 },
+        { { "wither_watcher", "pyrowatcher" }, keywords = { "test" }, weight = 150 },
+        { keywords = { "dante", "beyond", "hard", "demon", "demon2" }, weight = 300 },
+        { { "wither_watcher", "pyrowatcher" }, keywords = { "dante", "beyond", "hard", "demon", "demon2" }, weight = 150 },
+    },
+    attributes = {
+        resist = {
+            acid   = 50,
+            toxin  = 50,
+            ignite = 50,
+            bleed  = 50,
+        },
+        experience_value = 100,
+        health           = 200,
+    },
+    text = {
+        name      = "wither watcher",
+        namep     = "wither watchers",
+    },
+    callbacks = {
+        on_create = [=[
+        function( self )
+        end
+        ]=],
+        self_destruct = [=[
+            function( self )
+                if self:child("watcher_suicide") then
+                    world:destroy( self:child("watcher_suicide") )
+                    if self.health.current > 0 then self.health.current = 1 end
+                    local w = world:create_entity( "watcher_self_destruct" )
+                    world:attach( self, w )
+                    world:get_level():fire( self, world:get_position( self ), w, 200 )
+                end
+            end
+        ]=],
+        on_die = [=[
+            function( self, killer, current, weapon )
+                if self:child("watcher_suicide") then
+                    world:lua_callback( self, "self_destruct" )
+                end
+            end
+        ]=],
+        on_action = [=[
+            function( self )
+                local level = world:get_level()
+                if level:is_alive( self ) then
+                    for b in level:targets( self, 8 ) do
+                        if b.data then
+                            local data = b.data
+                            if data.ai and not data.is_mechanical then
+                                if data.ai.group == "player" or data.ai.group == "cri" then
+                                    world:add_buff( b, "buff_wither", 101, true )
+                                    core.apply_damage_status( b, "poisoned", "toxin", 1, self )
+                                else
+                                    world:add_buff( b, "buff_watcher_gaze_enemy", 101, true )
+                                end
+                            end
+                        end
+                    end
+                end
+                aitk.standard_ai( self )
+                if level:is_alive( self ) and self:child( "watcher_suicide" ) then
+                    local distance = level:distance( self, world:get_player() )
+                    if distance < 2 then
+                        world:lua_callback( self, "self_destruct" )
+                    end
+                end
+            end
+        ]=],
+        on_post_command = [=[
+            function ( actor, cmt, tgt, time )
+                local level = world:get_level()
+                for b in level:targets( actor, 8 ) do
+                    if b.data then
+                        local data = b.data
+                        if data.ai and not data.is_mechanical then
+                            if data.ai.group == "player" or data.ai.group == "cri" then
+                                world:add_buff( b, "buff_wither", 101, true )
+                                core.apply_damage_status( b, "poisoned", "toxin", 1, actor )
+                            else
+                                world:add_buff( b, "buff_watcher_gaze_enemy", 101, true )
+                            end
+                        end
+                    end
+                end
+            end
+        ]=],
+    },
 }
